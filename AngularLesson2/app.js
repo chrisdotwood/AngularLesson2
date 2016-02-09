118f507f55bf077aad759ff24e36cc5790de8c33
Chris Wood <chrisdotwood+github@gmail.com>
2016-02-09 13:18:59 +0000

    Loading message

--- AngularLesson2/app.js
+++ AngularLesson2/app.js
@@ -1,6 +1,21 @@
 ﻿/// <reference path="C:\Local Repositories\Interview practice\AngularLesson2\AngularLesson2\angular.js" />
 
 angular.module("flipApp", [])
-    .controller("flipController", function ($scope) {
+    .controller("flipController", function ($scope, $http) {
         $scope.title = "Flip - Reading Log";
+
+        $scope.people;
+        $scope.loading;
+
+        $http.get("http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&pretty=true&delay=2")
+            .success(function (data) {
+                $scope.people = data;
+                $scope.$emit("UNLOAD");
+            });
+
+        $scope.$on("LOAD", function () { $scope.loading = true });
+        $scope.$on("UNLOAD", function () { $scope.loading = false });
+
+        $scope.$emit("LOAD");
+
     });
