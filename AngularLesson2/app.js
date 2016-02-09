e719c8ed750b34d08a035ef4d1d0968da2f432ee
Chris Wood <chrisdotwood+github@gmail.com>
2016-02-09 12:42:30 +0000

    Initial checkin

---
+++ AngularLesson2/app.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="C:\Local Repositories\Interview practice\AngularLesson2\AngularLesson2\angular.js" />
+
+angular.module("flipApp", [])
+    .controller("flipController", function ($scope) {
+        $scope.title = "Flip - Reading Log";
+    });
---
+++ AngularLesson2/tests.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="C:\Local Repositories\Interview practice\AngularLesson2\AngularLesson2\app.js" />
+/// <reference path="C:\Local Repositories\Interview practice\AngularLesson2\AngularLesson2\qunit-1.21.0.js" />
+
+QUnit.test("Tests are working", function () {
+    var flip = angular.module('flipApp',[]);
+
+    flip.con
+});
